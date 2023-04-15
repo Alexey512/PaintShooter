2808d68d9e152c90a64f8f9da09a1f62532e2896
Alexey Pavlovskiy <palovskiy.alexey@gmail.com>
2023-04-16 00:10:34 +0300

    test network players

---
+++ Assets/Scripts/Game/NetShooter/BulletController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mirror;
+using UnityEngine;
+
+namespace Game.NetShooter
+{
+	public class BulletController: NetworkBehaviour
+	{
+		public float destroyAfter = 2;
+		public float speed = 10;
+
+		[SyncVar]
+		public Vector3 direction = Vector3.zero;
+
+		public override void OnStartServer()
+		{
+			Invoke(nameof(DestroySelf), destroyAfter);
+		}
+
+		// set velocity for server and client. this way we don't have to sync the
+		// position, because both the server and the client simulate it.
+		void Start()
+		{
+			//rigidBody.AddForce(transform.forward * force);
+		}
+
+		void Update()
+		{
+			transform.position += direction * speed * Time.deltaTime;
+		}
+
+		// destroy for everyone on the server
+		[Server]
+		void DestroySelf()
+		{
+			NetworkServer.Destroy(gameObject);
+		}
+
+		// ServerCallback because we don't want a warning
+		// if OnTriggerEnter is called on the client
+		[ServerCallback]
+		void OnTriggerEnter(Collider co) => DestroySelf();
+	}
+}
--- Assets/Scripts/Game/NetShooter/PlayerNetController.cs
+++ Assets/Scripts/Game/NetShooter/PlayerNetController.cs
@@ -5,21 +5,29 @@ using System.Text;
 using System.Threading.Tasks;
 using Mirror;
 using Mirror.Examples.Tanks;
+using TMPro;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.InputSystem;
 
 namespace Game.NetShooter
 {
 	public class PlayerNetController : NetworkBehaviour
-    {
-        [Header("Components")]
-        public NavMeshAgent agent;
+	{
+		[SerializeField]
+		private PlayerInput _input;
+	    
+	    [Header("Components")]
         public Animator  animator;
-        public TextMesh  healthBar;
+        public TextMeshPro  healthBar;
         public Transform turret;
+
+
+        public Rigidbody2D _body;
 
         [Header("Movement")]
         public float rotationSpeed = 100;
+        public float moveSpeed = 1;
 
         [Header("Firing")]
         public KeyCode shootKey = KeyCode.Space;
@@ -29,11 +37,11 @@ namespace Game.NetShooter
         [Header("Stats")]
         [SyncVar] public int health = 4;
 
-        void Update()
+        void FixedUpdate()
         {
             // always update health bar.
             // (SyncVar hook would only update on clients, not on server)
-            healthBar.text = new string('-', health);
+            healthBar.text = health.ToString();
             
             // take input from focused window only
             if(!Application.isFocused) return; 
@@ -41,24 +49,71 @@ namespace Game.NetShooter
             // movement for local player
             if (isLocalPlayer)
             {
+	            Vector2 inputVector = _input.actions["Move"].ReadValue<Vector2>();
+	            if (inputVector == Vector2.zero)
+	            {
+		            animator.SetBool("Moving", false);
+		            return;
+	            }
+
+	            Vector3 moveVector = new Vector3(inputVector.x, inputVector.y, 0.0f);
+
+                Debug.Log($"Move -> {moveVector.x}:{moveVector.y}");
+
                 // rotate
+                //float horizontal = moveVector.x;
+                //transform.Rotate(0, 0, horizontal * rotationSpeed * Time.deltaTime);
+
+                var targetRotation = Quaternion.LookRotation(Vector3.forward, moveVector);
+                transform.rotation =
+	                Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+                //transform.Rotate();
+
+                // move
+                float vertical = moveVector.y;
+                Vector3 forward = transform.TransformDirection(Vector3.forward);
+                Vector3 velocity = forward * Mathf.Max(vertical, 0) * moveSpeed;
+
+                //_body.velocity = velocity;
+
+                //_body.velocity = moveVector * moveSpeed;
+
+                //animator.SetBool("Moving", agent.velocity != Vector3.zero);
+
+                //transform.position += velocity * Time.deltaTime;
+
+                transform.position += moveVector * moveSpeed;
+
+                animator.SetBool("Moving", moveVector != Vector3.zero);
+
+	            /*
+	            // rotate
                 float horizontal = Input.GetAxis("Horizontal");
                 transform.Rotate(0, horizontal * rotationSpeed * Time.deltaTime, 0);
-
                 // move
                 float vertical = Input.GetAxis("Vertical");
                 Vector3 forward = transform.TransformDirection(Vector3.forward);
                 agent.velocity = forward * Mathf.Max(vertical, 0) * agent.speed;
                 animator.SetBool("Moving", agent.velocity != Vector3.zero);
-
+                */
+                
                 // shoot
-                if (Input.GetKeyDown(shootKey))
+                /*if (Input.GetKeyDown(shootKey))
                 {
                     CmdFire();
                 }
+                */
 
                 RotateTurret();
             }
+        }
+
+        public void OnShoot(InputAction.CallbackContext context)
+        {
+	        if (context.started)
+	        {
+		        CmdFire();
+	        }
         }
 
         // this is called on the server
@@ -66,6 +121,13 @@ namespace Game.NetShooter
         void CmdFire()
         {
             GameObject projectile = Instantiate(projectilePrefab, projectileMount.position, projectileMount.rotation);
+
+            var bullet = projectile.GetComponent<BulletController>();
+            if (bullet != null)
+            {
+	            bullet.direction = projectileMount.rotation * Vector3.right;
+            }
+
             NetworkServer.Spawn(projectile);
             RpcOnFire();
         }
@@ -80,7 +142,7 @@ namespace Game.NetShooter
         [ServerCallback]
         void OnTriggerEnter(Collider other)
         {
-            if (other.GetComponent<Projectile>() != null)
+            if (other.GetComponent<BulletController>() != null)
             {
                 --health;
                 if (health == 0)
@@ -90,13 +152,15 @@ namespace Game.NetShooter
 
         void RotateTurret()
         {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            /*
+	        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             if (Physics.Raycast(ray, out RaycastHit hit, 100))
             {
                 Debug.DrawLine(ray.origin, hit.point);
                 Vector3 lookRotation = new Vector3(hit.point.x, turret.transform.position.y, hit.point.z);
                 turret.transform.LookAt(lookRotation);
             }
+            */
         }
     }
 }
